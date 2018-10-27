95979ac7950243bd7101aa811605e47ab8da551f
Joshua <joshua.mulder@hotmail.com>
2018-10-28 00:17:38 +0200

    Test versie

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Product;
+use App\User;
 use App\Voorraad;
 use Illuminate\Http\Request;
 
@@ -11,9 +12,9 @@ class AdminController extends Controller
     public function index()
     {
         $productCount = Product::count();
-        $voorraadCount = Voorraad::count();
+        $gebruikers = User::count();
 
-        return view('admin/index', compact('productCount', 'voorraadCount'));
+        return view('admin/index', compact('productCount', 'gebruikers'));
     }
 
 }
